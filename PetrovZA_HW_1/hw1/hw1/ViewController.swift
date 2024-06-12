2d0c588675a11d5e53c9edbb2ea513825dc74fa4
zakhar terminal <zakharterminal@MacBook-Pro-zakhar.local>
2024-06-12 12:56:33 +0300

    added hw 1

---
+++ PetrovZA_HW_1/hw1/hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by zakhar terminal on 2.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
